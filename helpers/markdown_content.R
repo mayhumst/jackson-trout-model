1e5d52f6ceaed2e2046fb47526f98679d6d38656
mayhumst <mayahumston@gmail.com>
2025-06-27 14:19:50 -0400

    create markdown styled variables for description and attributions

---
+++ helpers/markdown_content.R
@@ -0,0 +1,71 @@
+#
+# This file contains the text content of the attributions and the site description.
+# These items are written and formatted using markdown language. See below for some common markdown formatting styles. 
+#
+#
+#       Text can be **bold** or *italicized.*
+#
+#       # This is a Header!
+#       ## Here's a subheader...
+#       ### ...And so on
+#
+#       Link to an external website like [this](https://www.youtube.com/watch?v=dQw4w9WgXcQ)
+#       Or, link to a header so the user can jump somewhere else on the page: 
+#       [the first header](#this-is-a-header) 
+#       [the second sub-header](#heres-a-subheader)
+#       [or the third](#and-so-on)
+#       This can be helpful for a table of contents.
+#
+#       - This is
+#       - a bulleted list
+#           - with sub-bullets
+#               - (indented by tabs)
+#
+#       1. The same can be done
+#       2. with numerical lists. 
+#           1. There are multiple ways
+#           - to subindent lists.
+#
+#       Separate two sections with a horizontal line break using three hyphens.
+#       ---
+#       Like that!
+#
+#       If you want to separate two paragraphs with a newline,
+#       you must use TWO carriage returns, 
+#       not just
+#       one. For example, this whole block of text will show up as 
+#       one cohesive paragraph. 
+#
+#       However, this is a new, separate paragraph!
+#
+#       Alternatively, you can use a newline character. This is paragraph 1....\nThis is paragraph 2!
+#
+#       Demonstrate small chunks of code with `code blocks` inline with your text. 
+#       For multiple lines of code, use three backticks instead. 
+#       Be sure to specify the language; you can do this for any coding language, even shell code!
+#       ```python
+#       # Small python program
+#       my_sum = 0
+#       for i in range(1,6):
+#           my_sum += i
+#       print("Done!")
+#       ```
+#       
+#
+
+
+
+attributions <- 
+"
+**Attributions:**
+Research and model by [Dr. Robert Humston,](https://www.wlu.edu/profile/humston-robert) Professor and Director of Environmental Studies at Washington and Lee University 
+
+Data from [USGS Water Services](https://waterservices.usgs.gov/)
+
+Website and application by Maya Humston
+"
+
+description <- 
+"
+Fill in here
+"
